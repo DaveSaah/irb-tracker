19ed701ba7abfe62d42ef7a6d3b405298d47abdb
David Saah <dasorange.hope@gmail.com>
2024-03-30 16:34:05 +0000

    feat: setup go server

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"io"
+	"text/template"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+// Templates struct for parsed html templates
+type Templates struct {
+	templates *template.Template
+}
+
+// Render renders html templates
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+// newTemplate creates a new template
+func newTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Static("assets"))
+	e.Renderer = newTemplate()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", nil)
+	})
+
+	// start server with logger
+	e.Logger.Fatal(e.Start(":42069"))
+}
