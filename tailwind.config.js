4ceafb2572eaedfa71ef1d433185de71ff6f2066
David Saah <dasorange.hope@gmail.com>
2024-03-31 13:15:12 +0000

    feat: define global custom color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./views/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-red": "#FF0307",
+        "primary-text": "#000908",
+        "primary-bg": "#FBFFFE",
+        "primary-blue": "#91A6FF",
+      },
+    },
   },
   plugins: [],
 };
