0d76f23fd8ca1d0c6eebe14a364cc627c29b34e9
David Saah <dasorange.hope@gmail.com>
2024-04-06 11:39:07 +0000

    chore: merge helper functions

--- helpers/check_mail.go
+++
@@ -1,8 +0,0 @@
-package helpers
-
-import "regexp"
-
-// IsMailValid checks if the email is valid or not.
-func IsMailValid(email string) bool {
-	return regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`).MatchString(email) || email == ""
-}
--- helpers/doc.go
+++
@@ -1,2 +0,0 @@
-// Package helpers defines the utility functions that are used across the application.
-package helpers
