0c8222a2c0d367d1202fedb042617a9bfe52ec57
David Saah <dasorange.hope@gmail.com>
2024-04-06 11:39:07 +0000

    chore: merge helper functions

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,10 +1,19 @@
+// Package helpers defines the utility functions that are used across the application.
 package helpers
 
 import (
 	"log"
+	"regexp"
 
 	"golang.org/x/crypto/bcrypt"
 )
+
+// IsMailValid checks if the email is valid or not.
+func IsMailValid(email string) bool {
+	return regexp.MustCompile(
+		`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`,
+	).MatchString(email) || email == ""
+}
 
 // getHashPassword returns a hashed password using bcrypt and default cost
 func GetHashPassword(password string) (string, error) {
