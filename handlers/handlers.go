bd1549091b1c50e6eb2ea68c701ea614e925eb96
David Saah <dasorange.hope@gmail.com>
2024-04-06 11:44:49 +0000

    feat: add session route guards

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,35 +3,76 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/boring-school-work/irb-tracker/helpers"
 	"github.com/labstack/echo/v4"
 )
 
 // IndexView renders the index page.
 func IndexView(c echo.Context) error {
+	sess, isLoggedIn := helpers.CheckSession(c)
+	if isLoggedIn {
+		return c.Render(http.StatusOK, "dashboard", sess)
+	}
+
 	return c.Render(http.StatusOK, "index", nil)
 }
 
 // HomeView renders the home page.
 func HomeView(c echo.Context) error {
+	sess, isLoggedIn := helpers.CheckSession(c)
+	if isLoggedIn {
+		return c.Render(http.StatusOK, "dashboard", sess)
+	}
+
 	return c.Render(http.StatusOK, "home", nil)
 }
 
 // DashboardView renders the dashboard page.
 func DashboardView(c echo.Context) error {
-	return c.Render(http.StatusOK, "dashboard", nil)
+	sess, isLoggedIn := helpers.CheckSession(c)
+	if !isLoggedIn {
+		return c.Render(http.StatusBadRequest, "index", nil)
+	}
+
+	return c.Render(http.StatusOK, "dashboard", sess)
 }
 
 // ProjectsView renders the projects page.
 func ProjectsView(c echo.Context) error {
+	_, isLoggedIn := helpers.CheckSession(c)
+	if !isLoggedIn {
+		return c.Render(http.StatusBadRequest, "index", nil)
+	}
+
 	return c.Render(http.StatusOK, "projects", nil)
 }
 
 // ActivityView renders the activity page.
 func ActivityView(c echo.Context) error {
+	_, isLoggedIn := helpers.CheckSession(c)
+	if !isLoggedIn {
+		return c.Render(http.StatusBadRequest, "index", nil)
+	}
+
 	return c.Render(http.StatusOK, "activity", nil)
 }
 
 // ReviewView renders the review page.
 func ReviewView(c echo.Context) error {
+	sess, isLoggedIn := helpers.CheckSession(c)
+	if !isLoggedIn {
+		return c.Render(http.StatusBadRequest, "index", nil)
+	}
+
+	if sess.Values["type"] != "faculty" {
+		return c.Render(http.StatusBadRequest, "dashboard", sess)
+	}
+
 	return c.Render(http.StatusOK, "review", nil)
+}
+
+// LogoutUser logouts out a user
+func LogoutUser(c echo.Context) error {
+	helpers.DestroySession(c)
+	return c.Render(http.StatusOK, "home", nil)
 }
