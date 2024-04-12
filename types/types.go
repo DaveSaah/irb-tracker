b59abc7acf5d41075cedf295ebf1101797a616d7
David Saah <dasorange.hope@gmail.com>
2024-04-12 19:48:41 +0000

    feat: add status types definition and counter function

---
+++ types/types.go
@@ -0,0 +1,12 @@
+package types
+
+const (
+	// StatusSubmitted is the status id for a submitted project
+	StatusSubmitted = 0
+	// StatusPending is the status id for a project pending approval
+	StatusPending = 1
+	// StatusRejrcted is the status id for a rejected project
+	StatusRejected = 4
+	// StatusApproved is the status id for an approved project
+	StatusApproved = 5
+)
