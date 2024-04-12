73a963249113ae9725dc2c2a95d9dbecefcb1124
David Saah <dasorange.hope@gmail.com>
2024-04-12 19:48:41 +0000

    feat: add status types definition and counter function

--- actions/submit_proposal.go
+++ actions/submit_proposal.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/boring-school-work/irb-tracker/db"
 	"github.com/boring-school-work/irb-tracker/model"
+	"github.com/boring-school-work/irb-tracker/types"
 )
 
 // SubmitProposal submits a proposal form
@@ -20,17 +21,17 @@ func SubmitProposal(p model.Project) error {
 
 	_, err = conn.Exec(
 		`INSERT INTO projects (
-    brief, title, associated_documents, principal_investigator,
+    brief, title, proposal, associated_documents, principal_investigator,
     dept, supervisor, start_date, end_date, other_investigators,
     purpose, results_dissemination, participants_count,
-    participants_type, recruitment_method
+    participants_type, recuitment_method, status_id
     ) 
     VALUES 
-    (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-		p.Brief, p.Title, p.DocumentPath, p.PrincipalID,
+    (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+		p.Brief, p.Title, p.Proposal, p.DocumentPath, p.PrincipalID,
 		p.DeptID, p.SupID, p.StartDate, p.EndDate, strings.Join(p.Investigators, ","),
 		strings.Join(p.Purpose, ","), strings.Join(p.ResultsDissemination, ","), p.ParticipantCount,
-		strings.Join(p.ParticipantType, ","), strings.Join(p.RecruitmentMethod, ","),
+		strings.Join(p.ParticipantType, ","), strings.Join(p.RecruitmentMethod, ","), types.StatusPending,
 	)
 	if err != nil {
 		log.Printf("Error inserting project: %s\n", err)
--- functions/counters.go
+++ functions/counters.go
@@ -1,20 +1,11 @@
 package functions
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/boring-school-work/irb-tracker/db"
-)
-
-const (
-	// StatusSubmitted is the status id for a submitted project
-	StatusSubmitted = 0
-	// StatusPending is the status id for a project pending approval
-	StatusPending = 1
-	// StatusRejrcted is the status id for a rejected project
-	StatusRejected = 4
-	// StatusApproved is the status id for an approved project
-	StatusApproved = 5
+	"github.com/boring-school-work/irb-tracker/types"
 )
 
 // GetProjectCount returns the number of projects in the database for a user
@@ -39,21 +30,50 @@ func GetProjectCount(userID, status int) (int, error) {
 
 	var count int
 
-	if status == StatusSubmitted {
+	if status == types.StatusSubmitted {
 		err = conn.QueryRow(`
     SELECT COUNT(*) FROM projects
-    INNER JOIN users
-    ON users.id = ?`, userID).Scan(&count)
+    WHERE principal_investigator = ?`, userID).Scan(&count)
 	} else {
 		err = conn.QueryRow(`
     SELECT COUNT(*) FROM projects
-    INNER JOIN users
-    ON users.id = ?
-    WHERE status_id = ?`,
+    WHERE principal_investigator = ?
+    AND status_id = ?`,
 			userID, status,
 		).Scan(&count)
 	}
+	if err != nil && err != sql.ErrNoRows {
+		log.Printf("Error getting project count: %v", err)
+		return 0, err
+	}
+
+	return count, nil
+}
+
+func GetProjectReviewCount(userID, status int) (int, error) {
+	conn, err := db.Init()
 	if err != nil {
+		log.Printf("Error initialising db: %v", err)
+		return 0, err
+	}
+
+	defer conn.Close()
+
+	var count int
+
+	if status == types.StatusSubmitted {
+		err = conn.QueryRow(`
+    SELECT COUNT(*) FROM projects
+    WHERE supervisor = ?`, userID).Scan(&count)
+	} else {
+		err = conn.QueryRow(`
+    SELECT COUNT(*) FROM projects
+    WHERE supervisor = ?
+    AND status_id = ?`,
+			userID, status,
+		).Scan(&count)
+	}
+	if err != nil && err != sql.ErrNoRows {
 		log.Printf("Error getting project count: %v", err)
 		return 0, err
 	}
